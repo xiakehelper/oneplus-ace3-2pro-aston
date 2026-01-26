e6fea70d98fee2043f61e519dc948a0d12a02ffd
xiakehelper <92614585+xiakehelper@users.noreply.github.com>
2026-01-27 00:06:24 +0800

    Modify firmware paths for OnePlus Aston A15
    Updated firmware paths for OnePlus Aston A15 in the script.

--- xigua-fw_squasher-a15.sh
+++ xigua-fw_squasher-a15.sh
@@ -3,13 +3,34 @@ cd pil-squasher
 make install
 
 rm -rf /firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/aston
-/usr/local/bin/pil-squasher $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mbn $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mdt
-rm -rf $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mdt $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.b*
 
-#cat $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b23_1 $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b23_2 > $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b23
-#cat $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b24_1 $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b24_2 > $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b24
-/usr/local/bin/pil-squasher $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mbn $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mdt
-rm -rf $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mdt $1/firmware-oneplus-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b*
+# adsp.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp.b*
+
+# adsp_dtb.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp_dtb.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp_dtb.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp_dtb.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/adsp_dtb.b*
+
+# cdsp.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp.b*
+
+# cdsp_dtb.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp_dtb.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp_dtb.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp_dtb.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/cdsp_dtb.b*
+
+# ipa_fws.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/ipa_fws.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/ipa_fws.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/ipa_fws.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/ipa_fws.b*
+
+# modem.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem.b*
+
+# modem_dtb.mbn
+/usr/local/bin/pil-squasher $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem_dtb.mbn $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem_dtb.mdt
+rm -rf $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem_dtb.mdt $1/firmware-oneplus-aston-a15/usr/lib/firmware/qcom/sm8550/xigua/modem_dtb.b*
 
 cd $1
 dpkg-deb --build --root-owner-group firmware-oneplus-a15
