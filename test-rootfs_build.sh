2137e1501d5378f36e6c03b5ead56efb62e431ec
xiakehelper <92614585+xiakehelper@users.noreply.github.com>
2026-01-27 23:16:38 +0800

    Add script to build and configure Ubuntu root filesystem
    This script builds a root filesystem image for Ubuntu, installs necessary packages, and configures the environment.

---
+++ test-rootfs_build.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+VERSION="25.04"
+
+cd $2
+
+truncate -s 1G rootfs.img
+mkfs.ext4 rootfs.img
+mkdir rootdir
+mount -o loop rootfs.img rootdir
+
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/$VERSION/release/ubuntu-base-$VERSION-base-arm64.tar.gz
+tar xzvf ubuntu-base-$VERSION-base-arm64.tar.gz -C rootdir
+
+mkdir -p rootdir/data/local/tmp
+mount --bind /dev rootdir/dev
+mount --bind /dev/pts rootdir/dev/pts
+mount --bind /proc rootdir/proc
+mount -t tmpfs tmpfs rootdir/data/local/tmp
+mount --bind /sys rootdir/sys
+
+echo "nameserver 1.1.1.1" | tee rootdir/etc/resolv.conf
+echo "oneplus-aston" | tee rootdir/etc/hostname
+echo "127.0.0.1 localhost
+127.0.1.1 oneplus-aston" | tee rootdir/etc/hosts
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:\$PATH
+export DEBIAN_FRONTEND=noninteractive
+
+chroot rootdir apt update
+chroot rootdir apt upgrade -y
+chroot rootdir apt install -y python3-defer
+
+echo "#!/bin/bash
+exit 0" | tee rootdir/var/lib/dpkg/info/python3-defer.postinst
+chroot rootdir dpkg --configure python3-defer
+
+chroot rootdir apt install -y bash-completion sudo ssh nano rmtfs qrtr-tools u-boot-tools- cloud-init- wireless-regdb- libreoffice*- transmission*- remmina*- $1
+
+echo "[Daemon]
+DeviceScale=2" | tee rootdir/etc/plymouth/plymouthd.conf
+
+echo "[org.gnome.desktop.interface]
+scaling-factor=2" | tee rootdir/usr/share/glib-2.0/schemas/93_hidpi.gschema.override
+
+echo "PARTLABEL=win / ext4 errors=remount-ro,x-systemd.growfs 0 1" | tee rootdir/etc/fstab
+
+echo 'ACTION=="add", SUBSYSTEM=="misc", KERNEL=="udmabuf", TAG+="uaccess"' | tee rootdir/etc/udev/rules.d/99-oneplus-aston.rules
+
+chroot rootdir glib-compile-schemas /usr/share/glib-2.0/schemas
+
+mkdir rootdir/var/lib/gdm
+touch rootdir/var/lib/gdm/run-initial-setup
+
+chroot rootdir pw-metadata -n settings 0 clock.force-quantum 2048
+
+# docker
+chroot rootdir bash <(curl -sSL https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh) \
+                --source mirrors.tencent.com/docker-ce \
+                --source-registry registry.hub.docker.com \
+                --protocol http \
+                --install-latest true \
+                --close-firewall true \
+                --ignore-backup-tips | awk '/脚本运行完毕，更多使用教程详见官网/ {exit} {print}'
+
+local daemon_json="rootdir/etc/docker/daemon.json"
+if [ ! -f "$daemon_json" ]; then
+   mkdir -p rootdir/etc/docker
+   echo "{}" > "$daemon_json"
+fi
+
+# docker end
+
+chroot rootdir apt clean
+
+umount rootdir/sys
+umount rootdir/proc
+umount rootdir/dev/pts
+umount rootdir/data/local/tmp
+umount rootdir/dev
+umount rootdir
+
+rm -d rootdir
+
+7z a rootfs.7z rootfs.img
